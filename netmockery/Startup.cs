14c34ab6e1d21a34d0991bb4ac653957ed6e1cdd
Bernt R. Brenna <bernt.brenna@gmail.com>
2016-09-14 12:25:50 +0000

    Refactored long method

--- netmockery/Startup.cs
+++ netmockery/Startup.cs
@@ -33,6 +33,40 @@ namespace netmockery
             services.AddMvc();
         }
 
+        public async Task HandleRequest(ResponseRegistryItem responseRegistryItem, HttpContext context, string requestBody, byte[] requestBodyBytes)
+        {
+            var endpoint = Program.EndpointCollection.Resolve(context.Request.Path.ToString());
+            responseRegistryItem.Endpoint = endpoint;
+            if (endpoint != null)
+            {
+                bool singleMatch;
+                var matcher_and_creator = endpoint.Resolve(context.Request.Path, requestBody, context.Request.Headers, out singleMatch);
+                if (matcher_and_creator != null)
+                {
+                    var responseCreator = matcher_and_creator.Item2;
+
+                    responseRegistryItem.RequestMatcher = matcher_and_creator.Item1;
+                    responseRegistryItem.ResponseCreator = responseCreator;
+                    responseRegistryItem.SingleMatch = singleMatch;
+
+                    if (responseCreator.Delay > 0)
+                    {
+                        await Task.Delay(TimeSpan.FromSeconds(responseCreator.Delay));
+                    }
+                    var responseBytes = await responseCreator.CreateResponseAsync(context.Request, requestBodyBytes, context.Response, endpoint.Directory);
+                    responseRegistryItem.ResponseBody = Encoding.UTF8.GetString(responseBytes);
+                }
+                else
+                {
+                    responseRegistryItem.Error = "Endpoint has no match for request";
+                }
+            }
+            else
+            {
+                responseRegistryItem.Error = "No endpoint matches request path";
+            }
+        }
+
         public void Configure(IApplicationBuilder app)
         {
             app.UseDeveloperExceptionPage();
@@ -50,6 +84,17 @@ namespace netmockery
                 context.Request.Body.CopyTo(memoryStream);
                 var requestBodyBytes = memoryStream.ToArray();
                 var requestBody = Encoding.UTF8.GetString(requestBodyBytes);
+
+                if (context.Request.Path.ToString() == "/")
+                {
+                    var indexFile = Path.Combine(Program.EndpointCollection.SourceDirectory, "index.html");
+                    if (File.Exists(indexFile))
+                    {
+                        var responseCreator = new FileResponse(indexFile) { ContentType = "text/html" };
+                        await responseCreator.CreateResponseAsync(context.Request, requestBodyBytes, context.Response, null);
+                        return;
+                    }
+                }
                 var responseRegistryItem = new ResponseRegistryItem
                 {
                     Timestamp = DateTime.Now,
@@ -59,36 +104,7 @@ namespace netmockery
 
                 try
                 {
-                    var endpoint = Program.EndpointCollection.Resolve(context.Request.Path.ToString());
-                    responseRegistryItem.Endpoint = endpoint;
-                    if (endpoint != null)
-                    {
-                        bool singleMatch;
-                        var matcher_and_creator = endpoint.Resolve(context.Request.Path, requestBody, context.Request.Headers, out singleMatch);
-                        if (matcher_and_creator != null)
-                        {
-                            var responseCreator = matcher_and_creator.Item2;
-
-                            responseRegistryItem.RequestMatcher = matcher_and_creator.Item1;
-                            responseRegistryItem.ResponseCreator = responseCreator;
-                            responseRegistryItem.SingleMatch = singleMatch;
-
-                            if (responseCreator.Delay > 0)
-                            {
-                                await Task.Delay(TimeSpan.FromSeconds(responseCreator.Delay));
-                            }
-                            var responseBytes = await responseCreator.CreateResponseAsync(context.Request, requestBodyBytes, context.Response, endpoint.Directory);
-                            responseRegistryItem.ResponseBody = Encoding.UTF8.GetString(responseBytes);
-                        }
-                        else
-                        {
-                            responseRegistryItem.Error = "Endpoint has no match for request";
-                        }
-                    }
-                    else
-                    {
-                        responseRegistryItem.Error = "No endpoint matches request path";
-                    }
+                    await HandleRequest(responseRegistryItem, context, requestBody, requestBodyBytes);
                 }
                 catch (Exception e)
                 {
