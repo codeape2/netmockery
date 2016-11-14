04e88af9750692a0a0cbd8ed50de95456905f57c
Bernt Brenna <bernt.brenna@gmail.com>
2016-11-14 09:29:27 +0100

    Endpoint collection example + configurable integration testing

---
+++ UnitTests/IntegrationTests.cs
@@ -0,0 +1,64 @@
+﻿using netmockery;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace UnitTests
+{
+    public class IntegrationTests
+    {
+        private ITestOutputHelper output;
+
+        public IntegrationTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+        private const string FILENAME = "configurations_to_test.txt";
+
+        [Fact]
+        public void KnownConfigurationsTestOK()
+        {
+            var configurationsToTest = new List<string>();
+            configurationsToTest.Add("examples/example1");
+            if (File.Exists(FILENAME))
+            {
+                configurationsToTest.AddRange(from line in File.ReadAllLines(FILENAME) where !string.IsNullOrEmpty(line) && !line.StartsWith("#") select line);
+            }
+
+            output.WriteLine("Configurations:");
+            foreach (var directory in configurationsToTest)
+            {
+                output.WriteLine($"    {directory}");
+            }
+            output.WriteLine("");
+
+            foreach (var directory in configurationsToTest)
+            {
+                CheckConfigdirectory(directory);
+            }
+        }
+        
+        public void CheckConfigdirectory(string directory)
+        {
+            Assert.True(Directory.Exists(directory), $"Directory {directory} does not exist");
+            Assert.True(EndpointTestDefinition.HasTestSuite(directory), $"Directory {directory} has not test suite");
+            var endpointCollection = EndpointCollectionReader.ReadFromDirectory(directory);
+            Assert.True(endpointCollection.Endpoints.Count() > 0, $"No endpoints defined in {directory}");
+
+            output.WriteLine(directory);
+            var tests = EndpointTestDefinition.ReadFromDirectory(directory);
+            foreach (var test in tests.Tests)
+            {
+                output.WriteLine(test.Name);
+                var result = test.ExecuteAsync(endpointCollection).Result;
+                output.WriteLine(result.ResultAsString);
+                Assert.True(result.OK, $"Test case {result.TestCase.Name}, message '{result.Message}'");
+            }
+        }
+    }
+}
