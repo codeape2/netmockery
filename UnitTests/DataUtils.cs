4a450c1057031a56c2341cc384a6ebdd30ff9e40
Bernt Brenna <bernt.brenna@gmail.com>
2016-12-08 13:48:11 +0100

    Unit test util class, used to provide data for various tests

---
+++ UnitTests/DataUtils.cs
@@ -0,0 +1,27 @@
+﻿using netmockery;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    public static class DataUtils
+    {
+        public static JSONEndpoint CreateSimpleEndpoint(string name, string file, string pathregex = ".")
+        {
+            return new JSONEndpoint
+            {
+                name = name,
+                pathregex = pathregex,
+                responses = new[] {
+                    new JSONResponse {
+                        match = new JSONRequestMatcher(),
+                        file = file
+                    }
+                }
+            };
+
+        }
+    }
+}
