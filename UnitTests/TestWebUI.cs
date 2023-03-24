12f6964ff615fb95547b11cb28d663fa3e2066f6
André S. Hansen <andre.ok@online.no>
2023-03-24 17:08:38 +0100

    Re-added stripped TestWebUI.cs

---
+++ UnitTests/TestWebUI.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using netmockery;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace UnitTests
+{
+    public class TestWebUI
+    {
+        private HttpClient client;
+        private EndpointCollectionProvider ecp;
+
+        public TestWebUI(ITestOutputHelper output)
+        {
+            ecp = new EndpointCollectionProvider("examples/example1");
+
+            var factory = new CustomWebApplicationFactory<Program>(ecp);
+            client = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false,
+            });
+        }
+
+        [Fact]
+        public async Task RootRedirectsToHome()
+        {
+            var response = await client.GetAsync("/");
+            Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+            Assert.Equal("/__netmockery/Home", response.Headers.Location.ToString());
+        }
+
+        [Fact]
+        public async Task HomeRedirectsToEndpoints()
+        {
+            var response = await client.GetAsync("/__netmockery/Home");
+            Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+            Assert.Equal("/__netmockery", response.Headers.Location.ToString());
+        }
+    }
+}
